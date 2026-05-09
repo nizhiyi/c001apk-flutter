13925d04e100df9038b10ca88acee4378df8f1f7
nizhiyi <83541749+nizhiyi@users.noreply.github.com>
2026-05-10 01:05:25 +0800

    Update topic_order_controller.dart

--- lib/pages/topic/topic_order_controller.dart
+++ lib/pages/topic/topic_order_controller.dart
@@ -3,7 +3,7 @@ import 'package:get/get.dart';
 import '../../pages/topic/topic_page.dart' show TopicSortType;
 
 class TopicOrderController extends GetxController {
-  Rx<TopicSortType> topicSortType = TopicSortType.DEFAULT.obs;
+  Rx<TopicSortType> topicSortType = TopicSortType.DATELINE.obs;
 
   void setTopicSortType(TopicSortType topicSortType) {
     this.topicSortType.value = topicSortType;
